2b56363099fa3c0cf2451471517cc1d7b80195ec
meltyness <86126050+meltyness@users.noreply.github.com>
2024-12-28 15:11:26 -0600

    tone that down a bit

--- src/main.rs
+++ src/main.rs
@@ -219,7 +219,7 @@ async fn tokio_main(custom_hostport: String,
         unsafe {
             ctrlc::set_handler(move || {
             ////println!("Ratchet Debug: Average dispatch time over {} RUNS: {:#?}", rt_get_runs(), Duration::from_secs_f64(rt_get_avg() / rt_get_runs()));
-            //println!("Ratchet Debug: total rate: {} RUNS / sec", RUNS / (Instant::now() - FIRST_RUN.unwrap()).as_secs_f64());
+            println!("Ratchet Debug: total rate: {} RUNS / sec", RUNS / (Instant::now() - FIRST_RUN.unwrap()).as_secs_f64());
                 exit(0);
             })
             .expect("Error setting Ctrl-C handler");
